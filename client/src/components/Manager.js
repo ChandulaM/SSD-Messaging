fb48be28904e4d2e73985a55bea1ab94ad4a620b
P.D.G.N.T.D. Dharmasinghe <dilhannadun777@gmail.com>
2022-11-16 15:54:54 +0530

    file upload done

--- client/src/components/Manager.js
+++ client/src/components/Manager.js
@@ -5,11 +5,19 @@ import styles from "../styles/Manager.module.css";
 import Message from "./Message";
 import axios from "axios";
 import baseUrl from "../config/baseUrl";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import storage from "../config/firebase";
+import { Button } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
 
 function Manager() {
   const [isManager, setIsManager] = useState(false);
   const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
   const [messages, setMessages] = useState([]);
+  //const [fileName, setFileName] = useState("");
+  const [progress, setProgress] = useState(0);
+  const [file, setFile] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getUserScopes() {
@@ -52,6 +60,42 @@ function Manager() {
     return messageSavedByUser;
   };
 
+  const fileHandler = (e) => {
+    setProgress(0);
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    if (file.length === 0) {
+      alert("Please select a file !");
+    } else {
+      console.log(file);
+      const storageRef = ref(storage, `/files/${file.type}/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const prog = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(prog);
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            (url) => console.log(url),
+            setLoading(false)
+          );
+        }
+      );
+    }
+  };
+
+  const ButtonStyle = { margin: "10px 10px" };
   return (
     <div className={styles.container}>
       {isManager ? (
@@ -68,15 +112,32 @@ function Manager() {
             Welcome {user.nickname}
           </div>
           <div className={styles.uploadDiv}>
-            <div className={styles.fileNameRow}>
+            {/* <div className={styles.fileNameRow}>
               <span className={styles.uploadDivText}>File Name</span>
-              <input type="text" className={styles.fileNameInput}></input>
-            </div>
+              <input
+                type="text"
+                onChange={(e) => setFileName(e.target.value)}
+                className={styles.fileNameInput}
+              ></input>
+            </div> */}
             <div className={styles.fileSelectRow}>
               <span className={styles.uploadDivText}>Select file</span>
-              <input type="file" className={styles.fileSelectRow}></input>
+              <input
+                type="file"
+                onChange={fileHandler}
+                className={styles.fileSelectRow}
+              ></input>
             </div>
-            <button className={styles.uploadBtn}>Upload File</button>
+            <Button
+              loading={loading}
+              onClick={(e) => handleUpload(e)}
+              color="green"
+              appearance="primary"
+              style={ButtonStyle}
+            >
+              Upload File
+            </Button>
+            <h3>Uploaded {progress} %</h3>
           </div>
           <div className={styles.messagesDiv}>
             {messages.map((message) => {
